b126f49c4c3a2fb96e216c4b2203c4bc37135317
Rishikesh Mishra <148183580+rishikeshh09@users.noreply.github.com>
2024-05-28 09:54:15 +0530

    Update Renderer.java

--- src/Renderer.java
+++ src/Renderer.java
@@ -18,4 +18,24 @@ public class Renderer extends JPanel
 		Pong.pong.render((Graphics2D) g);
 	}
 
+}package pong;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		Pong.pong.render((Graphics2D) g);
+	}
+
 }
